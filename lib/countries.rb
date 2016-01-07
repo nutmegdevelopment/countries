fd7d222ecba9c12dc6ae7530d0017434bfea6454
Adit <a.saxena.email@gmail.com>
2016-01-07 18:00:26 +0000

    - Select countries now have an optout option to decide which country to exclude (eg. USA) - Add jurisdictions

--- lib/countries.rb
+++ lib/countries.rb
@@ -3,4 +3,11 @@ require "countries/version"
 require 'iso3166'
 
 class Country < ISO3166::Country
+
+  ACCEPTED_JURISDICTIONS = [
+    'GBR','AUT','AUS','BRB','BEL','BGR','CAN','IMN','CZE','DNK','EST','FIN','FRA',
+    'DEU','GRC','HKG','HUN','ISL','ITA','JPN','LVA','LIE','LTU','LUX','NLD','NZL',
+    'NOR','PRT','POL','IRL','ROU','SGP','SVK','SVN','ESP','SWE','CHE',
+  ]
+
 end

21be0c4e45e6ec9405570497ef0513bd2a5c998e
Adit <a.saxena.email@gmail.com>
2016-02-11 11:32:07 +0000

    added method to retrieve dropdown from countries

--- lib/countries.rb
+++ lib/countries.rb
@@ -3,4 +3,16 @@ require "countries/version"
 require 'iso3166'
 
 class Country < ISO3166::Country
+
+  # Returns eg. [{ id: 'GBR', name: 'United Kingdom'}, ..., { id: 'ITA', name: 'Italy'}, ...]
+  def self.iso3_to_dropdown(starting_countries = nil)
+    starting_countries ||= {
+      'United Kingdom' => 'GBR',
+    }
+    countries = starting_countries.merge(Country::Alpha3_NameIndex)
+    countries.map do |v,k|
+      { id: k, name: v }
+    end
+  end
+
 end
